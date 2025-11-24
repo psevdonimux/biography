2b1da44c45a850124329ceb3439d1d509c5185e0
psevdonimux <mail_451@mail.ru>
2025-11-24 18:13:10 +0300

    Рефакторинг кода

---
+++ js/ads-modal.js
@@ -0,0 +1,30 @@
+const img = new Image();
+img.src = 'images/avatar_animation.gif';
+img.onload = () => document.getElementById('avatar').src = img.src;
+const ads = document.getElementById('modalAds');
+const timer = document.getElementById('timer');
+ads.showModal();
+let timeLeft = 10;
+timer.textContent = timeLeft;
+const countdown = setInterval(() => {
+	timeLeft--;
+	timer.textContent = timeLeft;
+	if(timeLeft == 0){
+		clearInterval(countdown);
+		ads.close();
+	}
+}, 1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/index.js
+++ js/index.js
@@ -17,48 +17,18 @@ alert('Вернусь: 01.12.2025');
 // alert('Вернусь: 13.07.2026');
 var descriptions = [];
 const clickHandler = (id, text, str, timeout) => {
- const old = document.getElementById(str).innerHTML;
- document.getElementById(str).textContent = text;
- setTimeout(() => {
-  document.getElementById(str).innerHTML = old;
-  attachAllHandlers();
- }, timeout * 1000);
+	const old = document.getElementById(str).innerHTML;
+	document.getElementById(str).textContent = text;
+	setTimeout(() => {
+		document.getElementById(str).innerHTML = old;
+		attachAllHandlers();
+	}, timeout * 1000);
 };
 const description = (id, text, str, timeout) => document.getElementById(id).onclick = () => clickHandler(id, text, str, timeout);
 const attachAllHandlers = () => descriptions.forEach(item => description(item.id, item.text, item.str, item.timeout));
 const init = async () => {
- const response = await fetch('info.json');       
- descriptions = await response.json();   
- attachAllHandlers();
+	const response = await fetch('config/info.json');
+	descriptions = await response.json();
+	attachAllHandlers();
 };
 init();
-const img = new Image();
-img.src = 'images/avatar_animation.gif'; 
-img.onload = () => document.getElementById('avatar').src = img.src;
-const ads = document.getElementById('modalAds');
-const timer = document.getElementById('timer');
-/*ads.showModal();
-let timeLeft = 10;
-timer.textContent = timeLeft;
-const countdown = setInterval(() => {
- timeLeft--;
- timer.textContent = timeLeft;
- if(timeLeft == 0){
-  clearInterval(countdown);
-  ads.close();
- }
-}, 1000);*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
