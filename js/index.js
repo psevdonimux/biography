3ea001cb91e5bd2901ad750e3ef6a0f71e5e22d6
psevdonimux <87897213+psevdonimux@users.noreply.github.com>
2025-12-25 01:57:08 +0300

    Uncomment alert messages in index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
- alert('Вернусь: 29.12.2025');
+
 // alert('Вернусь: 12.01.2026');
 // alert('Вернусь: 26.01.2026');
 // alert('Вернусь: 09.02.2026');
@@ -33,4 +33,5 @@ init();
 const img = new Image();
 img.src = 'images/avatar_animation.gif'; 
 img.onload = () => document.getElementById('avatar').src = img.src;
+
 
